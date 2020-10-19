d1f11c80391d5922fb98ca43f6d23666aa1e293e
Shri <thisshri@gmail.com>
2020-10-19 19:18:42 +0530

    Add banner card

---
+++ src/components/BannerCard/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './styles.scss';
+
+const BannerCard = ({
+  image,
+  title,
+  description,
+  link,
+  linkText,
+}) => (
+  <div className="bannerCard">
+    <img alt="" src={image}></img>
+    <p>
+      {title}
+    </p>
+    <p>
+      {description}
+      {link && " "}
+      {
+        link &&
+        <a href={link}>
+          {linkText}
+        </a>
+      }
+    </p>
+  </div>
+)
+
+export default BannerCard;
--- src/containers/CampK12/index.js
+++ src/containers/CampK12/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import './styles.scss';
 
-const CampK12 = () => (
-  <h1>Mainpage</h1>
-)
+import GroupImg from 'static/group-40.svg';
+import HomeImg from 'static/home.svg';
+import MedalImg from 'static/medal.svg';
+import TabletImg from 'static/tablet.svg';
+
+import BannerCard from 'components/BannerCard';
+
+const CampK12 = () => { 
+  return (
+    <div className="online-courses">
+      <h1>
+        ONLINE COURSES
+      </h1>
+      <div>
+        <BannerCard
+          image={GroupImg}
+          title="Learn interactively."
+          description="LIVE online sessions with our expert mentors."
+          link="www.google.com"
+          linkText="See a demo."
+        />
+        <BannerCard
+          image={HomeImg}
+          title="Learn from anywhere"
+          description="Convenience and safety for you and your child."
+        />
+        <BannerCard
+          image={MedalImg}
+          title="Learn from the pioneers."
+          description="We've been teaching kids to code since 2010."
+          link="www.google.com"
+          linkText="Read more."
+        />
+        <BannerCard
+          image={TabletImg}
+          title="Learn by doing."
+          description="100% project-based curriculum. Solve real-world problems."
+      />
+      </div>
+      <button>
+        Book a free trial
+      </button>
+    </div>
+  )
+};
 
 export default CampK12;
