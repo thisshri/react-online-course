8e7ed4ea55b12201b81d586efa56f321b6059d08
Shri <thisshri@gmail.com>
2020-10-19 16:00:54 +0530

    Setup project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.scss';
 
+import {
+  HashRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import CampK12 from 'containers/CampK12';
+
 function App() {
   return (
-    <h1>Fo</h1>
+    <HashRouter>
+      <Switch>
+        <Route path="/" component={CampK12}/>
+      </Switch>
+    </HashRouter>
   );
 }
 
---
+++ src/containers/CampK12/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.scss';
+
+const CampK12 = () => (
+  <h1>Mainpage</h1>
+)
+
+export default CampK12;
