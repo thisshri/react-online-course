d1aa6f697425c74ebf5db9a88910b1355aa5b9b5
Shri <thisshri@gmail.com>
2020-10-22 10:51:56 +0530

    Updates

---
+++ precache-manifest.f58ee0e498a898e5f769acd46d94d4a3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b84cb023a2dc4aaa6605e0a013df81",
+    "url": "/assignment-react-camp-k12/index.html"
+  },
+  {
+    "revision": "cb4f8461b62531671568",
+    "url": "/assignment-react-camp-k12/static/css/main.6d3d68fe.chunk.css"
+  },
+  {
+    "revision": "d222b54cdb12fc4c7ec1",
+    "url": "/assignment-react-camp-k12/static/js/2.ef7bf53c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/assignment-react-camp-k12/static/js/2.ef7bf53c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb4f8461b62531671568",
+    "url": "/assignment-react-camp-k12/static/js/main.9e1d8a7d.chunk.js"
+  },
+  {
+    "revision": "7d1114d65d5b4258dc48",
+    "url": "/assignment-react-camp-k12/static/js/runtime-main.7b417192.js"
+  },
+  {
+    "revision": "4ff6c7b7c0655c46361851643a1aa716",
+    "url": "/assignment-react-camp-k12/static/media/camp-k-12-logo.4ff6c7b7.svg"
+  },
+  {
+    "revision": "15b2a5c835fd85d194e6e6edc0281128",
+    "url": "/assignment-react-camp-k12/static/media/community.15b2a5c8.svg"
+  },
+  {
+    "revision": "9aa55bf5b09800787a26b1a1905c2a96",
+    "url": "/assignment-react-camp-k12/static/media/group-40.9aa55bf5.svg"
+  },
+  {
+    "revision": "af3da035ca01c02dc4024f4023a9cd9f",
+    "url": "/assignment-react-camp-k12/static/media/growth.af3da035.svg"
+  },
+  {
+    "revision": "fd6f5eee232e433e950f113a2f5dc1be",
+    "url": "/assignment-react-camp-k12/static/media/header-background.fd6f5eee.svg"
+  },
+  {
+    "revision": "4cc25fa34d3882fdd41be138e6d3235f",
+    "url": "/assignment-react-camp-k12/static/media/home.4cc25fa3.svg"
+  },
+  {
+    "revision": "a51f3e4adaf85cc5a3b2c3db10025b53",
+    "url": "/assignment-react-camp-k12/static/media/leaderboard.a51f3e4a.svg"
+  },
+  {
+    "revision": "5d5c5d80779eb1414c551548dfaef52e",
+    "url": "/assignment-react-camp-k12/static/media/medal.5d5c5d80.svg"
+  },
+  {
+    "revision": "b9ea7a575a5f1d449e86ee4970de8286",
+    "url": "/assignment-react-camp-k12/static/media/offer.b9ea7a57.svg"
+  },
+  {
+    "revision": "39884cb87e1936475044bb1899964184",
+    "url": "/assignment-react-camp-k12/static/media/rupees.39884cb8.svg"
+  },
+  {
+    "revision": "cd465efd2b76e58728a2bb10c1b47659",
+    "url": "/assignment-react-camp-k12/static/media/tablet.cd465efd.svg"
+  },
+  {
+    "revision": "2ef608dc8307d93b3a040108562bdb4a",
+    "url": "/assignment-react-camp-k12/static/media/timer.2ef608dc.svg"
+  }
+]);
